6605d698d521ea13ce8385eb3c3d540f74f2ae58
Robert Coleman <github@robert.net.nz>
2014-05-23 13:20:52 +1200

    Version bump to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'github@robert.net.nz'
 license          'MIT'
 description      'Installs/Configures php-redis'
 long_description 'Installs/Configures php-redis'
-version          '0.1.0'
+version          '0.1.1'
 
 recommends 'php'
